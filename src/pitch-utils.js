c5e5f555ee801c9f81ee1278a0a17bb4639618e5
Glen Pike <ski@dijitl.co.uk>
2017-12-21 00:27:33 +0000

    consts in pitch-utils

--- src/pitch-utils.js
+++ src/pitch-utils.js
@@ -1,14 +1,14 @@
-function getPitch(note) {
-    var G0 = 24.5; //G0 = note number 0  = 24.5Hz
+const getPitch = (note) => {
+    const G0 = 24.5; //G0 = note number 0  = 24.5Hz
     if (note > 88)
         //hurts too much if any more!
         note = 88;
 
-    var pitch = G0 * Math.pow(2.0, 1.0 * note / 12);
+    const pitch = G0 * Math.pow(2.0, 1.0 * note / 12);
 
     //console.log('getPitch ', note, pitch);
     return pitch;
-}
+};
 
 module.exports = {
     getPitch,
